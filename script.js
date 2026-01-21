d92f6ae4f728675fc8f004868e0a44944d6f37c8
OraclesTales <uria.c.c@gmail.com>
2026-01-21 19:33:26 +0000

    created canvas and shapes in p5

---
+++ script.js
@@ -0,0 +1,23 @@
+function setup() {
+    createCanvas(400, 400);
+}
+function draw() {
+    background("lavender");
+    
+    
+
+ 
+
+    fill(0);
+    textAlign("center");
+    textSize(25);  
+    text("Canad'sexports", 200,200);
+    // fill(200);/
+    // rect(70, 20, 50, 14);
+    // if (mouseIsPressed === true) {
+    //     fill(170, 175, 200);
+    // } else {
+    //     fill("lightyellow");
+    // }
+    // circle(mouseX, mouseY, 25);
+}
