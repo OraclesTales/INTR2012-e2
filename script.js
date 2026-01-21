e5cd8edaf5e038fdf880ad94d987ec0521d8ad8e
OraclesTales <uria.c.c@gmail.com>
2026-01-21 19:58:25 +0000

    added data to p5 canvas

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
+let list = [];
+function preload(){
+    list = loadJSON("data.json");
+}
 function setup() {
     createCanvas(400, 400);
 }
 function draw() {
     background("lavender");
-    
-    
-
- 
-
     fill(0);
     textAlign("center");
     textSize(25);  
-    text("Canad'sexports", 200,200);
+    text("Canada's Exports", 200, 40);
     // fill(200);/
     // rect(70, 20, 50, 14);
     // if (mouseIsPressed === true) {
@@ -20,4 +19,13 @@ function draw() {
     //     fill("lightyellow");
     // }
     // circle(mouseX, mouseY, 25);
+
+    Object.values (list).map((item, index) => {
+        fill("lightpink");
+        rect(50, index * 30 + 65, item.amount, 10);
+        fill (0);
+        textSize(12);
+        textAlign("left");
+        text(item.name, 50, index * 30 + 60);
+    });
 }
